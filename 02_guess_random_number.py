b2c310afd5e968386f6ecfd24380160f880dc320
Matteo Preda <33986831+predamatteo@users.noreply.github.com>
2022-10-04 16:39:02 +0200

    Update 02_guess_random_number.py

--- 02_guess_random_number.py
+++ 02_guess_random_number.py
@@ -1,20 +1,32 @@
+"""
+You need to keep the import of random for generating random number.
+At line 13 you can modify the range in between the random number is generated
+"""
+
 import random
 
 print("Welcome to Random Number Guess")
 print("Number goes to 1 from 25")
+
 run = True
+
 randomNum = random.randint(1,25)
 guessNum = 0
 x = 0
+
 while(run):
+  
   guessNum = int(input("Digit a number: "))
+  
   if guessNum == randomNum:
     print("Good!!")
     print("You have guessed the number in ",x," times")
     run = False
     break
+    
   elif guessNum > randomNum:
     print("Too High!")
+    
   elif guessNum < randomNum:
     print("Too Low!")
   x+=1
